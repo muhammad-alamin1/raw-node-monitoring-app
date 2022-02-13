c29903c57ed5e7e7a0ce9cd0deb33d3c2748ca7d
muhammad <muhammad@kali.kali>
2022-02-13 20:45:54 +0600

    get checks

--- handlers/routeHandler/checkHandler.js
+++ handlers/routeHandler/checkHandler.js
@@ -137,7 +137,41 @@ handler._check.post = (requestProperties, callback) => {
 };
 
 handler._check.get = (requestProperties, callback) => {
-    // 
+    // check id is valid
+    const id = typeof(requestProperties.queryStringObj.id) === 'string'
+        && requestProperties.queryStringObj.id.trim().length === 25
+        ? requestProperties.queryStringObj.id
+        : false;
+
+    if(id){
+        // lookup the check for 
+        data.read('checks', id, (err, checkData) => {
+            if(!err && checkData) {
+                // check verify authentication token
+                let token = typeof(requestProperties.headerObj.token) === 'string'
+                    ? requestProperties.headerObj.token : false;
+
+                tokenHandler._token.verify(token, parseJSON(checkData).userPhone, (tokenIsValid) =>{
+                    if(tokenIsValid) {
+                        callback(200, parseJSON(checkData));
+                    } else {
+                        callback(403, {
+                            error: `Authentication Failed.!`
+                        })
+                    }
+                })
+                                
+            } else {
+                callback(500, {
+                    error: `Problem in our server side.!`
+                })
+            }
+        });
+    } else {
+        callback(400, {
+            error: `You have a problem in your request. Please try again!`
+        })
+    }
 };
 
 handler._check.put = (requestProperties, callback) => {
